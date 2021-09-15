c7a10f9ba552f13ff2ff97700a6811cfa489c831
romanpovzyk <roman.povzyk@gmail.com>
2021-09-15 23:48:16 +0300

    telegram_bot

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ lang = ''
 @bot.message_handler(commands=['start'])
 def start_message(message):
     keyboard = telebot.types.ReplyKeyboardMarkup(True)
-    keyboard.row('/start', '/uk', '/en', '/ru', '/help')
+    keyboard.row('/uk', '/en', '/ru')
     bot.send_message(message.chat.id, messages['general']['start'], reply_markup=keyboard)
 
 
